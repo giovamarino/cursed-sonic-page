8521087388aa25ac864a34d4696a8cf5e72200dd
Giovanni Marino <GiovaMarino1@gmail.com>
2023-01-09 18:58:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// window.onload = () => {
+//     alert("Welcome!!!");
+// }
+
+// let intervalID;
+
+// function repeatEverySecond() {
+//   intervalID = setInterval(sendMessage, 5000);
+// }
+
+// function sendMessage() {
+//     alert("Welcome!!!")
+//   console.log("One second elapsed");
+// }
+
+// repeatEverySecond()
+
+// setInterval(alert("Welcome!!!"), 500)
+
+// setInterval(alert("Welcome!!!"), 100)
